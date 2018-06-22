bb8924b04482b10447d0a2bc60ffe69d104862c2
Dominik Piatek <do.piatek@gmail.com>
2018-06-22 21:08:47 +0100

    Add ghost docker script

---
+++ run-ghost.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Remove old ghost
+docker rm -f ghost
+
+# Create new one
+docker run --name ghost \
+ -p 127.0.0.1:2368:2368 \
+ -e url=$url \
+ -v /home/ubuntu/ghost/content:/var/lib/ghost/content \
+ --restart=always \
+  -d ghost:1.25.4
