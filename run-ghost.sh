e6e18de794aebd86f620de8f2f04ba71e0c97422
Dominik Piatek <do.piatek@gmail.com>
2018-06-22 21:10:20 +0100

    Use env var

--- run-ghost.sh
+++ run-ghost.sh
@@ -6,7 +6,7 @@ docker rm -f ghost
 # Create new one
 docker run --name ghost \
  -p 127.0.0.1:2368:2368 \
- -e url=$url \
+ -e url=$URL \
  -v /home/ubuntu/ghost/content:/var/lib/ghost/content \
  --restart=always \
   -d ghost:1.25.4
